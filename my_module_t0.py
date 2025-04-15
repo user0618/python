0c0881cef9ba833ea7ae9aeae08f0b520bb3d1de
user0618 <whitecloudscu@yahoo.com.tw>
2025-04-15 12:02:09 +0800

    Create my_module_t0.py

---
+++ my_module_t0.py
@@ -0,0 +1,12 @@
+def say_hello(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+class Greeter:
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        return f"Hi, I'm {self.name}!"
